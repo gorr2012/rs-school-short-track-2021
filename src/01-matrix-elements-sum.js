8a365a5beedba866eb0ef1cf07ee03b3bd4882b0
gorr2012 <sergey_gornost@mail.ru>
2021-04-08 13:29:38 +0300

    feat: implement fn getMatrixElementsSum

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,26 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const arr = matrix.flat();
+  let res = 0;
+  if (arr.indexOf(0) !== -1) {
+    for (let i = matrix[0].length; i < arr.length; i++) {
+      if (matrix.length === 1 && matrix[0].length === 1) res = arr[i];
+      if (arr[i] === 0) {
+        if (Math.floor(i / matrix[0].length) >= 1) {
+          let j = (Math.floor(i / matrix[0].length));
+          while (j >= 1) {
+            res += arr[i - matrix[0].length * j];
+            j--;
+          }
+        }
+      }
+    }
+  } else {
+    res = arr.reduce((s, p) => s + p);
+  }
+  return res;
 }
 
 module.exports = getMatrixElementsSum;
