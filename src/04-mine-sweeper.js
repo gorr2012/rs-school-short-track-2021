a61b87731b533208814d15d1a8b8e22a82816755
gorr2012 <sergey_gornost@mail.ru>
2021-04-09 15:47:31 +0300

    feat: implement fn minesweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,41 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  let subarray = [];
+  const res = [];
+  let count = 0;
+  for (let j = 0; j < matrix.length; j++) {
+    for (let i = 0; i < matrix[j].length; i++) {
+      if (matrix[j][i + 1] === true || matrix[j][i - 1] === true) count += 1;
+      if (j < matrix.length - 1) {
+        if (matrix[j + 1][i] === true
+          || matrix[j + 1][i + 1] === true
+          || matrix[j + 1][i - 1] === true) {
+          count += 1;
+        }
+      }
+      if (j < matrix.length - 1 && j !== 0) {
+        if (matrix[j - 1][i] === true
+          || matrix[j - 1][i + 1] === true
+          || matrix[j - 1][i - 1] === true) {
+          count += 1;
+        }
+      }
+      if (j === matrix.length - 1) {
+        if (matrix[j - 1][i] === true
+          || matrix[j - 1][i + 1] === true
+          || matrix[j - 1][i - 1] === true) {
+          count += 1;
+        }
+      }
+      subarray.push(count);
+      count = 0;
+    }
+    res.push(subarray);
+    subarray = [];
+  }
+  return res;
 }
 
 module.exports = minesweeper;
