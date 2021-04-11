19407b19ffb207e9d544c4511c5a5428be7d5d0f
gorr2012 <sergey_gornost@mail.ru>
2021-04-11 20:30:28 +0300

    feat: implement fn encodeLine

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const res = [];
+  const strNew = str.split('');
+  for (let i = 0; i < strNew.length; i++) {
+    let j = i + 1;
+    let count = 1;
+    while (j < strNew.length) {
+      if (strNew[i] === strNew[j]) {
+        count++;
+        j++;
+      } else {
+        break;
+      }
+    }
+    if (res.find((e) => e === strNew[i]) === undefined) res.push(count, strNew[i]);
+    if (res[res.length - 1] !== strNew[i]) res.push(count, strNew[i]);
+  }
+  return res.filter((e) => e !== 1).join('');
 }
 
 module.exports = encodeLine;
