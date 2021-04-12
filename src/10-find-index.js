e624bdcbf30eb1890f015b9b8376e62118864e43
gorr2012 <sergey_gornost@mail.ru>
2021-04-12 16:23:09 +0300

    feat: implement fn findIndex

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,14 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value, start = 0, end = array.length - 1) {
+  const middle = Math.floor((start + (end - start) / 2));
+  if (end < 1) return array[0];
+  if (value === array[middle]) return middle;
+  if (end - 1 === start) return value === array[start] ? start : end;
+  if (value < array[middle]) return findIndex(array, value, start, middle);
+  if (value > array[middle]) return findIndex(array, value, middle, end);
+  return 'Error';
 }
 
 module.exports = findIndex;
